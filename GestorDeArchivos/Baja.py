06f44b489f23bde4126698f7e572bf48bf75dc37
Jorgeone1 <jorgeifpdam@gmail.com>
2023-11-15 14:21:33 +0100

    actualizacion
    he puesto algunos font en los label y el prototipo de un boton redondeado

--- GestorDeArchivos/Baja.py
+++ GestorDeArchivos/Baja.py
@@ -2,15 +2,15 @@ from tkinter import *
 
 pantallaBaja = Tk()
 
-pantallaBaja.geometry("500x500")
+pantallaBaja.geometry("550x300")
 
-Label(pantallaBaja,text="CODIGO EMPLEADO").grid(row=0,column=0,padx=30,pady=30)
-Label(pantallaBaja,text="FECHA BAJA").grid(row=0,column=1,padx=30,pady=30)
+Label(pantallaBaja,text="CODIGO EMPLEADO", font=("Microsoft Sans Serif", 8, "bold")).grid(row=0,column=0,padx=(90,30),pady=(30,0))
+Label(pantallaBaja,text="FECHA BAJA", font=("Microsoft Sans Serif", 8, "bold")).grid(row=0,column=1,padx=(40,80),pady=(30,0))
 
-Entry(pantallaBaja).grid(row=1,column=0)
-Entry(pantallaBaja).grid(row=1,column=1)
+Entry(pantallaBaja,width=25).grid(row=1,column=0,padx=(90,30))
+Entry(pantallaBaja,width=25).grid(row=1,column=1,padx=(40,80))
 
-Text(pantallaBaja,height=5,width=50).grid(row=2,column=0,columnspan=2,padx=30)
+Label(pantallaBaja,height=3,width=30,borderwidth=3, relief="solid",text="Mensaje", font=("Microsoft Sans Serif", 12, "bold"),fg="red").grid(row=2,column=0,columnspan=2,pady=(30,0))
 
-Button(pantallaBaja,text="CONFIRMAR").grid(row=3,column=0,columnspan=2,padx=30)
+Button(pantallaBaja,text="CONFIRMAR").grid(row=3,column=0,columnspan=2,padx=30,pady=(30,0))
 pantallaBaja.mainloop()
--- GestorDeArchivos/nominas.py
+++ GestorDeArchivos/nominas.py
@@ -1,5 +1,21 @@
 from tkinter import *
 from tkinter import font as tkFont
+from PIL import Image, ImageDraw, ImageTk
+
+def create_rounded_button_image(width, height, corner_radius, background_color, text_color, text):
+    # Crear una imagen PIL con fondo transparente
+    img = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(img)
+
+    # Dibujar un botón redondeado en la imagen
+    draw.ellipse((0, 0, width, height), fill=background_color)
+    draw.text((width // 2, height // 2), text, fill=text_color, anchor="mm")
+
+    # Convertir la imagen PIL en una imagen de PhotoImage de tkinter
+    rounded_button_image = ImageTk.PhotoImage(img)
+
+    return rounded_button_image
+
 pantallaAlta = Tk()
 
 pantallaAlta.geometry("1000x500")
@@ -7,17 +23,17 @@ customFont = tkFont.Font(family="Helvetica", size=9,weight="bold")
 
 #Primera fila
 
-Label(pantallaAlta,text="CÓDIGO").grid(row=0,column=0,padx=(5,0))
-Label(pantallaAlta,text="APELLIDOS Y NOMBRES").grid(row=0,column=2,columnspan=8)
+Label(pantallaAlta,text="CÓDIGO",font=("Microsoft Sans Serif", 8, "bold")).grid(row=0,column=0,padx=(5,0))
+Label(pantallaAlta,text="APELLIDOS Y NOMBRES",font=("Microsoft Sans Serif", 8, "bold")).grid(row=0,column=2,columnspan=8)
 #Segunda Fila
 
 Entry(pantallaAlta,width=15).grid(row=1,column=0,padx=(5,0))
 Entry(pantallaAlta).grid(row=1,column=2,columnspan=8,sticky="ew")
 
 #Tercera Fila
-Label(pantallaAlta,text="FECHA NACIMIENTO").grid(row=2,column=0,columnspan=2,padx=(15,0),pady=10)
-Label(pantallaAlta,text="FECHA ALTA").grid(row=2,column=3,padx=(0,30))
-Label(pantallaAlta,text="DIRECCIÓN").grid(row=2,column=4,columnspan=7)
+Label(pantallaAlta,text="FECHA NACIMIENTO",font=("Microsoft Sans Serif", 8, "bold")).grid(row=2,column=0,columnspan=2,padx=(15,0),pady=10)
+Label(pantallaAlta,text="FECHA ALTA",font=("Microsoft Sans Serif", 8, "bold")).grid(row=2,column=3,padx=(0,30))
+Label(pantallaAlta,text="DIRECCIÓN",font=("Microsoft Sans Serif", 8, "bold")).grid(row=2,column=4,columnspan=7)
 
 #Cuarta Fila
 Entry(pantallaAlta).grid(row=3,column=0,columnspan=2,padx=(15,0))
@@ -25,44 +41,47 @@ Entry(pantallaAlta).grid(row=3,column=3,padx=(0,30))
 Entry(pantallaAlta).grid(row=3,column=4,columnspan=7,sticky="ew")
 
 #QUINTA FILA
-Label(pantallaAlta,text="NIF").grid(row=4,column=0,padx=(30,0),pady=10)
-Label(pantallaAlta,text="DATOS BANCARIOS").grid(row=4,column=3,columnspan=4)
-Label(pantallaAlta,text="NÚMERO AFILICIACIÓN SS").grid(row=4,column=7,columnspan=3)
+Label(pantallaAlta,text="NIF",font=("Microsoft Sans Serif", 8, "bold")).grid(row=4,column=0,padx=(30,0),pady=10)
+Label(pantallaAlta,text="DATOS BANCARIOS",font=("Microsoft Sans Serif", 8, "bold")).grid(row=4,column=3,columnspan=4)
+Label(pantallaAlta,text="NÚMERO AFILICIACIÓN SS",font=("Microsoft Sans Serif", 8, "bold")).grid(row=4,column=7,columnspan=3)
 
 #sexta Fila
 Entry(pantallaAlta).grid(row=5,column=0,padx=(30,0))
 Entry(pantallaAlta).grid(row=5,column=3,columnspan=4,sticky="ew",padx=(0,20))
 Entry(pantallaAlta).grid(row=5,column=7,columnspan=3,sticky="ew")
 #Septima FILA
-Label(pantallaAlta,text="SALARIO BRUTO").grid(row=6,column=0,columnspan=2,padx=(30,0),pady=20)
+Label(pantallaAlta,text="SALARIO BRUTO",font=("Microsoft Sans Serif", 8, "bold")).grid(row=6,column=0,columnspan=2,padx=(30,0),pady=20)
 Entry(pantallaAlta).grid(row=6,column=3)
-Label(pantallaAlta,text="NUMERO PAGAS").grid(row=6,column=4)
-Entry(pantallaAlta).grid(row=6,column=5)
+Label(pantallaAlta,text="NUMERO PAGAS",font=("Microsoft Sans Serif", 8, "bold")).grid(row=6,column=4)
+Entry(pantallaAlta).grid(row=6,column=6)
 #novena fila
-Label(pantallaAlta,text="SALARIO MES").grid(row=8,column=0,padx=(10,0),pady=20,columnspan=2)
+Label(pantallaAlta,text="SALARIO MES",font=("Microsoft Sans Serif", 8, "bold")).grid(row=8,column=0,padx=(10,0),pady=20,columnspan=2)
 
 Entry(pantallaAlta).grid(row=8,column=2,columnspan=2,sticky="ew")
-Label(pantallaAlta,text="%IRPF").grid(row=8,column=4,padx=(20,0))
+Label(pantallaAlta,text="%IRPF",font=("Microsoft Sans Serif", 8, "bold")).grid(row=8,column=4,padx=(20,0))
 Label(pantallaAlta,text="  ").grid(row=8,column=5)
 Entry(pantallaAlta).grid(row=8,column=6,sticky="ew")
-Label(pantallaAlta,text="RETENCION IRPF").grid(row=8,column=7,padx=(20,0))
+Label(pantallaAlta,text="RETENCION IRPF",font=("Microsoft Sans Serif", 8, "bold")).grid(row=8,column=7,padx=(20,0))
 Label(pantallaAlta,text="     ").grid(row=8,column=8)
 Entry(pantallaAlta).grid(row=8,column=9,sticky="ew")
 #decima fila
-Label(pantallaAlta,text="PRORRATA PAGAS").grid(row=9,column=0,padx=(10,0),columnspan=2)
+Label(pantallaAlta,text="PRORRATA PAGAS",font=("Microsoft Sans Serif", 8, "bold")).grid(row=9,column=0,padx=(10,0),columnspan=2)
 Entry(pantallaAlta).grid(row=9,column=2,columnspan=2,sticky="ew")
-Label(pantallaAlta,text="SEG.SOCIAL").grid(row=9,column=4,padx=(20,0))
+Label(pantallaAlta,text="SEG.SOCIAL",font=("Microsoft Sans Serif", 8, "bold")).grid(row=9,column=4,padx=(20,0))
 Entry(pantallaAlta).grid(row=9,column=6,sticky="ew")
-Label(pantallaAlta,text="DEDUCCION SS").grid(row=9,column=7,padx=(20,0))
+Label(pantallaAlta,text="DEDUCCION SS",font=("Microsoft Sans Serif", 8, "bold")).grid(row=9,column=7,padx=(20,0))
 Entry(pantallaAlta).grid(row=9,column=9,sticky="ew")
 
 #undecima fila
 Text(pantallaAlta,height=3).grid(row=10,column=0,columnspan=7,sticky="ew",padx=(30,0),pady=15)
-Label(pantallaAlta,text="CONFIRMAR").grid(row=10,column=7,sticky="ew")
+Label(pantallaAlta,text="CONFIRMAR",font=("Microsoft Sans Serif", 8, "bold")).grid(row=10,column=7,sticky="ew")
 Entry(pantallaAlta).grid(row=10,column=8,columnspan=2,sticky="ew")
 
-Button(pantallaAlta,text="CARGAR EMPLEADO").grid(row=11,column=0,columnspan=7)
-Button(pantallaAlta,text="CALCULAR").grid(row=11,column=7)
-Button(pantallaAlta,text="IMPRIMIR").grid(row=11,column=9)
+rounded_button_image = create_rounded_button_image(100, 40, 5, "yellow", "black", "Click Me")
+font = ("Helvetica", 10, "bold")
+Button(pantallaAlta,text="CARGAR EMPLEADO",font=("Microsoft Sans Serif", 8, "bold"),relief="ridge",borderwidth=3).grid(row=11,column=0,columnspan=7)
+Button(pantallaAlta,text="CALCULAR",font=("Microsoft Sans Serif", 8, "bold"),width=20,height=3,relief="ridge").grid(row=11,column=7)
+Button(pantallaAlta,image=rounded_button_image,font=font).grid(row=11,column=9)
+
 
 pantallaAlta.mainloop()
