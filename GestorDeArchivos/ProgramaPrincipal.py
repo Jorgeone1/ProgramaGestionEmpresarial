e40cda1d88b7cbe3f61d632f1cc3b30efa36a083
Jorgeone1 <jorgeifpdam@gmail.com>
2023-11-16 10:16:20 +0100

    creacion base de datos
    cree la base de datos para la gestion

--- GestorDeArchivos/ProgramaPrincipal.py
+++ GestorDeArchivos/ProgramaPrincipal.py
@@ -1,16 +1,45 @@
 import tkinter as tk
+import sqlite3 as sql
 
-root = tk.Tk()
+conexion = sql.connect("empleado.db")
 
-# Entry superior con un ancho específico
-entry1 = tk.Entry(root, width=20)
-entry1.grid(row=0, column=0, sticky="ew", padx=(0, 0))  # Alineado a la izquierda y derecha, sin padding adicional.
+cursor = conexion.cursor()
+tabla = '''CREATE TABLE IF NOT EXISTS Empleados (
+    ID INTEGER primary key autoincrement,
+    Nombre VARCHAR(50) not null,
+    fechaNacimiento Date not null,
+    FechaAlta DATE not null,
+    Direccion varchar(150) not null,
+    NIF varchar(9) not null,
+    DatosBancarios varchar(24) not null,
+    NAF varchar(12) not null,
+    genero varchar(20) not null,
+    departamento varchar(50) not null,
+    puesto varchar(50) not null,
+    telefono int(9) not null,
+    email varchar(100) not null,
+    salario float not null,
+    pagasExtra float not null,
+    irpf float not null,
+    seguridadsocial int(9) not null,
+    Altas boolean not null
+);'''
+cursor.execute(tabla)
 
-# Entry inferior con un ancho mayor
-entry2 = tk.Entry(root, width=30)
-entry2.grid(row=1, column=0, sticky="ew", padx=(0, 0))  # Alineado a la izquierda y derecha, sin padding adicional.
+conexion.commit()
+datos = ("Jorge","2002-12-5","2009-05-02","Parla villaverde alto 12 4 izq","12345678E","ES213123123412123456789",121234567812,"Hombre","informatica","secretario",659839212,"paco@gmaicl.com",120.20,20.50,10,123456789,True)
+print(len(datos))
+insertarDatos="Insert into empleados(nombre,fechanacimiento,fechaalta,direccion,nif,datosbancarios,naf,genero,departamento,puesto,telefono,email,salario,pagasExtra,irpf,seguridadsocial,altas) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+cursor.execute(insertarDatos,datos)
+conexion.commit()
+consulta = "Select * from empleados"
+cursor.execute(consulta)
 
-# Configura la columna 0 para que se expanda y llene el espacio si la ventana cambia de tamaño
-root.grid_columnconfigure(0, weight=1)
+# Obtener todos los resultados de la consulta
+resultados = cursor.fetchall()
 
-root.mainloop()
+# Imprimir los resultados
+for resultado in resultados:
+    print(resultado)
+
+conexion.close()
